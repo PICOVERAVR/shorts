fc7afb5584f66917167b85891f9404795c8bc03f
Kyle Neil <kyle@johnneil.net>
2022-02-12 23:10:11 -0800

    added flame graph support

--- perf.sh
+++ perf.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+SVG_VIEWER=brave
+
 if ! command -v perf &> /dev/null
 then
 	echo "cannot find perf executable!"
@@ -9,10 +11,11 @@ fi
 if test $# -lt 3;
 then
 	echo "usage: ./perf.sh <profile type> <count?> <program to profile> <program arguments>
-	available profiling types:
-		hardware: reports CPU hardware counters for the entire program
-		hardware_rep <n>: repeat n times
-		execute: shows a breakdown of execution time for the entire program
+    available profiling types:
+        hardware: reports CPU hardware counters for the entire program
+        hardware_rep <n>: repeat n times
+        execute: shows a breakdown of execution time for the entire program
+        flamegraph: generates a flamegraph of the execution time and opens the result in $SVG_VIEWER
 	"
 	exit
 fi
@@ -20,6 +23,7 @@ fi
 case $1 in
 	hardware)
 		shift
+		echo "NOTE: The final column indicates how much time a counter was active for (due to # events > # counters)."
 		perf stat -d -d -d "$@"
 		;;
 	hardware_rep)
@@ -29,7 +33,17 @@ case $1 in
 		;;
 	execute)
 		shift
-		perf record "$@" && perf report && rm perf.data
+		perf record "$@" && perf report --hierarchy && rm perf.data*
+		;;
+	flame)
+		# install the "cargo-flamegraph" package on Linux
+		if ! command -v flamegraph &> /dev/null
+		then
+			echo "cannot find flamegraph executable!"
+			exit 1
+		fi
+		shift
+		flamegraph "$@" && $SVG_VIEWER flamegraph.svg && rm flamegraph.svg && rm perf.data*
 		;;
 	*)
 		echo "unknown option!"
